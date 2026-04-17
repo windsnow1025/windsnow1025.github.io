516c7dee94c74246d24e4d6bf4db1bee31efabb7
windsnow1025 <windsnow1025@gmail.com>
2026-04-16 23:09:01 -0400

    chore: main format

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import {ThemeProvider, createTheme} from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import "./global.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import './global.css'
+import App from './App.tsx'
 
 const theme = createTheme({
   colorSchemes: {light: true, dark: true},
-});
+})
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
       <App/>
     </ThemeProvider>
   </StrictMode>,
-);
+)
